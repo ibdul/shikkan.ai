85f5aa346375953316cf90aad16ea3bbb8e2b4a5
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-02-24 00:45:04 +0100

    remove testimonials and update video section title

--- pages/index.js
+++ pages/index.js
@@ -17,17 +17,11 @@ const Home = () => {
       </div>
       <Benefits imgPos="right" data={benefitTwo} />
       <SectionTitle
-        pretitle="Watch a review"
-        title="See what a happy user thinks of us">
+        pretitle="A Quick Demo"
+        title="Learn About Us in a Quick Video">
         A picture is worth more than a thousand words they say, a video is definitely worth even more than.
       </SectionTitle>
       <Video />
-      <SectionTitle
-        pretitle="Testimonials"
-        title="Here's what our customers said">
-        Get a feel of what our users think about us
-      </SectionTitle>
-      <Testimonials />
       <SectionTitle pretitle="FAQs" title="Frequently Asked Questions">These are questions we frequently get.</SectionTitle>
       <Faq />
       <Cta />
